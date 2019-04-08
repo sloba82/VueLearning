824257046f182d1602522f3eff54981460283125
sloba82 <dizajnistampa@gmail.com>
2019-04-08 08:04:43 +0200

    forms element v-model

--- section 11/forms/src/App.vue
+++ section 11/forms/src/App.vue
@@ -19,7 +19,8 @@
                                 type="password"
                                 id="password"
                                 class="form-control"
-                                v-model="userData.password">
+                                v-model.lazy="userData.password">
+                                <p>{{ userData.password }}</p>
                     </div>
                     <div class="form-group">
                         <label for="age">Age</label>
@@ -39,7 +40,9 @@
                     <textarea
                             id="message"
                             rows="5"
-                            class="form-control"></textarea>
+                            class="form-control"
+
+                            v-model="message"></textarea>
                 </div>
             </div>
             <div class="row">
@@ -49,13 +52,15 @@
                             <input
                                     type="checkbox"
                                     id="sendmail"
-                                    value="SendMail"> Send Mail
+                                    value="SendMail"
+                                    v-model="sendMail"> Send Mail
                         </label>
                         <label for="sendInfomail">
                             <input
                                     type="checkbox"
                                     id="sendInfomail"
-                                    value="SendInfoMail"> Send Infomail
+                                    value="SendInfoMail"
+                                    v-model="sendMail"> Send Infomail
                         </label>
                     </div>
 
@@ -67,13 +72,15 @@
                         <input
                                 type="radio"
                                 id="male"
-                                value="Male"> Male
+                                value="Male"
+                                v-model="gender"> Male
                     </label>
                     <label for="female">
                         <input
                                 type="radio"
                                 id="female"
-                                value="Female"> Female
+                                value="Female"
+                                v-model="gender"> Female
                     </label>
                 </div>
             </div>
@@ -82,8 +89,10 @@
                     <label for="priority">Priority</label>
                     <select
                             id="priority"
-                            class="form-control">
-                        <option></option>
+                            class="form-control"
+                            v-model="selectedPriority">
+                        <option v-for="priority in priorities">
+                            {{ priority }}</option>
                     </select>
                 </div>
             </div>
@@ -107,13 +116,13 @@
                         <p>Mail: {{ userData.email }}</p>
                         <p>Password:{{ userData.password}}</p>
                         <p>Age:{{ userData.age }}</p>
-                        <p>Message: </p>
+                        <p style="white-space: pre">Message: {{ message }} </p>
                         <p><strong>Send Mail?</strong></p>
                         <ul>
-                            <li></li>
+                            <li v-for="mails in sendMail"> {{  mails }}</li>
                         </ul>
-                        <p>Gender:</p>
-                        <p>Priority:</p>
+                        <p>Gender: {{gender}} </p>
+                        <p>Priority: {{ selectedPriority }}</p>
                         <p>Switched:</p>
                     </div>
                 </div>
@@ -130,7 +139,13 @@
                     email:'',
                     password: '',
                     age: 27,
-                }
+
+                },
+                 message: 'Text message',
+                 sendMail: [],
+                 gender:'Male',
+                 priorities: [ 'High', 'Medium', 'Low'],
+                 selectedPriority: 'High',
             }
         },
 
