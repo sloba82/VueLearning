19082a1b3ed7ce8fa8edc3bf31fee051024e663f
sloba82 <dizajnistampa@gmail.com>
2019-05-18 18:25:56 +0200

    http

--- Section 15/Http via vue-resource/src/App.vue
+++ Section 15/Http via vue-resource/src/App.vue
@@ -3,6 +3,15 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Http</h1>
+                <div class="form-group">
+                	<label>username</label>
+                	<input  class="form-control" type="text" v-model="user.username">
+                </div> 
+                <div class="form-group">
+                	<label>email</label>
+                	<input  class="form-control" type="text" v-model="user.email">
+                </div> 
+                <button class="btn btn-primary" @click="submit" >Submit</button>
             </div>
         </div>
     </div>
@@ -10,6 +19,29 @@
 
 <script>
     export default {
+    	data(){
+    		return {
+    			user: {
+    				username:'',
+    				email:'',
+    			}
+    		};
+    	},
+
+    	methods: {
+    		submit(){
+ 
+    			console.log(this.user);
+
+    			this.$http.post('https://test-f0c3b.firebaseio.com/data.json', this.user)
+    			.then(response =>{
+    				console.log(response);
+    			}, error => {
+    				console.log(error);
+    			});
+
+    		}
+    	}
     }
 </script>
 
--- Section 15/Http via vue-resource/src/main.js
+++ Section 15/Http via vue-resource/src/main.js
@@ -3,7 +3,7 @@ import VueResource from 'vue-resource';
 import App from './App.vue'
 
 
-Vue.use();
+Vue.use(VueResource);
 
 new Vue({
   el: '#app',
