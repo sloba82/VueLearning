effb9bec9c832b8b8b6fe701e2e8939db5f7e17d
Sloba82 <dizajnistampa@gmail.com>
2019-05-20 15:30:11 +0200

    custom resources

---
+++ Section 15/Http via vue-template_urls/src/App.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Http</h1>
+                <div class="form-group">
+                	<label>username</label>
+                	<input  class="form-control" type="text" v-model="user.username">
+                </div> 
+                <div class="form-group">
+                	<label>email</label>
+                	<input  class="form-control" type="text" v-model="user.email">
+                </div> 
+                <button class="btn btn-primary" @click="submit" >Submit</button>
+                <hr>
+                <button class="btn btn-primary" @click="fetchData" >Get Data</button>
+
+                <ul class="list-group">
+                    <li class="list-group-item" v-for="u in users">{{ u.username }} - {{ u.email }} </li>
+
+                </ul>
+ 
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+    	data(){
+    		return {
+    			user: {
+    				username:'',
+    				email:'',
+    			},
+ 
+                users: [],
+                resource: {},
+    		};
+    	},
+
+    	methods: {
+    		submit(){
+ 
+/*    			console.log(this.user);
+
+    			this.$http.post('', this.user)
+    			.then(response =>{
+    				console.log(response);
+    			}, error => {
+    				console.log(error);
+    			});*/
+
+
+          this.resource.saveAlt({}, this.user); 
+       /*     this.resource.save({}, this.user);  */ 
+
+
+
+    		},
+
+            fetchData(){
+                this.$http.get('data.json')
+                .then(responce => {
+                    return responce.json();
+                })
+                .then(data => {
+                    const resultArray = [];
+                    for (let key in data) {
+                        resultArray.push(data[key]);
+                    }
+                    this.users = resultArray;
+                });
+
+
+
+            },
+    	},
+
+        created(){
+          const customActions = {
+                saveAlt: {method: 'POST', url: 'alternative.json'}
+            };
+
+            this.resource = this.$resource('data.json', {}, customActions);
+        } 
+    }
+</script>
+
+<style>
+</style>
