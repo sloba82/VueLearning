be9802c47715bba41d2478a303f2d081e983018c
Sloba82 <dizajnistampa@gmail.com>
2019-06-06 09:23:54 +0200

    vue router

---
+++ Section 16/seting_up_vue_router/src/routes.js
@@ -0,0 +1,9 @@
+import Home from './components/Home.vue'
+import User from './components/user/User.vue'
+
+
+export const router = [
+	{path: '' , component: Home},
+	{path: '/user' , component: User},
+
+];
