9379d436ec393b8fb3f305eda10736bf6387b434
sloba82 <dizajnistampa@gmail.com>
2019-06-16 18:17:50 +0200

    router links

--- Section 16/seting_up_vue_router/src/App.vue
+++ Section 16/seting_up_vue_router/src/App.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="container">
+    	
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Routing</h1>
                 <hr>
+                <app-heder></app-heder>
                 <router-view></router-view>
 
             </div>
@@ -12,7 +14,13 @@
 </template>
 
 <script>
+
+	import Heder from './components/Heder.vue';
     export default {
+
+    	  components: {
+    		appHeder: Heder,
+    	},
     }
 </script>
 
--- Section 16/seting_up_vue_router/src/main.js
+++ Section 16/seting_up_vue_router/src/main.js
@@ -5,7 +5,11 @@ import {routes} from './routes';
 
 Vue.use(VueRouter);
 
-const router = new VueRouter({ routes });
+const router = new VueRouter({ 
+	routes,
+	mode:'history'
+
+});
 
 new Vue({
   el: '#app',
