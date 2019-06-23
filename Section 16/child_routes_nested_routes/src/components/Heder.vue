885589fc8dc54988d2c81d43bd0dfec295c436c7
sloba82 <dizajnistampa@gmail.com>
2019-06-23 22:38:57 +0200

    router links

---
+++ Section 16/child_routes_nested_routes/src/components/Heder.vue
@@ -0,0 +1,16 @@
+<template>
+	<ul class="nav nav-pills">
+	  <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
+	  <router-link to="/user/1" tag="li" active-class="active" exact><a>User 1</a></router-link>
+	  <router-link to="/user/2" tag="li" active-class="active" exact><a>User 2</a></router-link>
+	</ul>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style>
+</style>
+
