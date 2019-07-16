0ff2a9ba414873475f260f2e8a02065fbb81e90b
sloba82 <dizajnistampa@gmail.com>
2019-07-16 19:11:51 +0200

    setup

---
+++ Section 16/multiple_router/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+    	
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Routing</h1>
+                <hr>
+                <app-heder></app-heder>
+                <router-view name="header-top"></router-view>
+                <router-view></router-view>
+                <router-view name="header-bottom"></router-view>
+
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+	import Heder from './components/Heder.vue';
+    export default {
+
+    	  components: {
+    		appHeder: Heder,
+    	},
+    }
+</script>
+
+<style>
+</style>
---
+++ Section 16/multiple_router/src/components/user/User.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<h1>The User Page</h1>
+	    <hr>
+	    <p>Loaded ID: {{ id }}</p>
+	    <button @click="navigateToHome" class="btn btn-primary">Go to home</button>
+	    <hr>
+	    <router-view></router-view>
+
+	</div>
+</template>
+
+<script>
+	export default {
+		methods: {
+			navigateToHome(){
+				this.$router.push({ name: 'home'});
+			}
+		}
+	}
+
+</script> 
