ae49a1927d99c5b28a533458138f3bbd5a80f634
sloba82 <dizajnistampa@gmail.com>
2019-07-27 12:48:36 +0200

    redirect

---
+++ Section 16/redirecting_router/src/components/user/UserEdit.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<h3>Edit the User</h3>
+		<p>Locale: {{$route.query.locale }}</p>
+		<p>Analitics: {{$route.query.q }}</p>
+
+	</div>
+</template>
+
+
+<script>
+	export default {
+		created(){
+			console.log(this);
+		},
+	}
+
+</script> 
